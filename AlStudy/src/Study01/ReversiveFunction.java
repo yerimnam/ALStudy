485638eb927963d6349883adbfd3123dde6a5019
Yerim <dpfla725@naver.com>
2023-01-26 21:59:57 +0900

    20230126_재귀함수

---
+++ AlStudy/src/Study01/ReversiveFunction.java
@@ -0,0 +1,17 @@
+package Study01;
+
+public class ReversiveFunction {
+	   public static void recursiveFunction(int i) {
+	        // 100번째 호출을 했을 때 종료되도록 종료 조건 명시
+	        if (i == 100) return;
+	        System.out.println(i + "번째 재귀 함수에서 " + (i + 1) + "번째 재귀함수를 호출합니다.");
+	        recursiveFunction(i + 1); //다음 재귀함수 호출 (for문의 i++)
+	        System.out.println(i + "번째 재귀 함수를 종료합니다.");
+	    }
+
+	    public static void main(String[] args) {
+	        recursiveFunction(1);
+	    }
+
+
+}
