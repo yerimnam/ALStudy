88417531bc416b22468ddcceebae4b5f8cb816b5
Yerim <dpfla725@naver.com>
2023-01-31 22:54:02 +0900

    20230131_미로찾기,BFS

---
+++ AlStudy/src/Study01/Node.java
@@ -0,0 +1,23 @@
+package Study01;
+
+
+public class Node {
+
+    private int x;
+    private int y;
+
+    public Node(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return this.x;
+    }
+    
+    public int getY() {
+        return this.y;
+    }
+}
+
+
